c002e1fd2aff4c2361b31ea74b8d0d9e48aab38b
ESWAR SATHA <sathaeswar2003@gmail.com>
2025-09-11 05:30:56 +0000

    add multiple options to login like via google,facebook etc

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,6 +24,21 @@ import { Logo } from '@/components/icons';
 import { Eye, EyeOff, Globe, LogIn } from 'lucide-react';
 import { languages, useLanguage } from '@/lib/locales/language-context';
 
+const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <title>Google</title>
+        <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.85 3.18-1.73 4.1-1.05 1.05-2.58 2.6-5.5 2.6-4.8 0-8.48-3.98-8.48-8.88s3.68-8.88 8.48-8.88c2.79 0 4.95.99 6.59 2.5l-2.22 2.22c-1.05-.99-2.43-1.63-4.37-1.63-3.6 0-6.52 2.94-6.52 6.56s2.92 6.56 6.52 6.56c4.17 0 5.8-2.84 6.04-4.38h-6.14z" />
+    </svg>
+);
+
+const FacebookIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <title>Facebook</title>
+        <path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.35C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.732 0 1.325-.593 1.325-1.325V1.325C24 .593 23.407 0 22.675 0z" />
+    </svg>
+);
+
+
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
   const { language, setLanguage, translations } = useLanguage();
@@ -113,6 +128,26 @@ export default function LoginPage() {
                         <LogIn className="mr-2"/>
                         {translations.login_page.login_seller_button}
                     </Link>
+                </Button>
+            </div>
+            <div className="relative my-2">
+                <div className="absolute inset-0 flex items-center">
+                    <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                    <span className="bg-card px-2 text-muted-foreground">
+                    Or continue with
+                    </span>
+                </div>
+            </div>
+             <div className="grid grid-cols-2 gap-4">
+                <Button variant="outline">
+                    <GoogleIcon className="mr-2 h-4 w-4" />
+                    Google
+                </Button>
+                <Button variant="outline">
+                    <FacebookIcon className="mr-2 h-4 w-4 fill-current" />
+                    Facebook
                 </Button>
             </div>
           </div>
